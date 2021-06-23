658308ef31e33cc464b9a1f2599f288ccd68beb5
Youcef Ouarab <hy_ouarab@esi.dz>
2021-06-23 23:58:24 +0100

    refactor: changed logo

--- app/src/main/java/com/example/medicana/App.kt
+++ app/src/main/java/com/example/medicana/App.kt
@@ -1,9 +1,10 @@
 package com.example.medicana
 
 import android.app.Application
+import androidx.multidex.MultiDexApplication
 import com.example.medicana.room.RoomService
 
-class App: Application() {
+class App: MultiDexApplication() {
     override fun onCreate() {
         super.onCreate()
         RoomService.context = applicationContext
